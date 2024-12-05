a1d2d15a833c3c1124a928fec8a78014aef3a16f
abubnov-sf <andrii.b@scalefinal.com>
2024-12-05 16:32:06 +0100

    Reduce `connected` events number

--- client/src/components/RegisterDeviceSheet.tsx
+++ client/src/components/RegisterDeviceSheet.tsx
@@ -2,21 +2,22 @@ import { useState } from 'react';
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet.tsx';
 import { Button } from '@/components/ui/button.tsx';
 import { RegisterDeviceForm } from '@/components/RegisterDeviceForm.tsx';
-import { useDeviceRegister } from '@/hooks/useDeviceRegister.ts';
 import { RegisterDeviceSchemaType } from '@/schemas.ts';
 import { Plus } from 'lucide-react';
+import { useMutation } from '@tanstack/react-query';
+import { registerDevice } from '@/api/registerDevice.ts';
 
 export const RegisterDeviceSheet = () => {
 	const [isOpen, setIsOpen] = useState(false);
-	const registerDeviceMutation = useDeviceRegister();
+	const registerDeviceMutation = useMutation({ mutationFn: registerDevice });
 
 	const onSubmit = (form: RegisterDeviceSchemaType) => {
-		registerDeviceMutation(form);
+		registerDeviceMutation.mutate(form);
 		setIsOpen(false);
 	};
 	return (
 		<Sheet open={isOpen} onOpenChange={setIsOpen}>
-			<SheetTrigger className="bg-transparent hover:border-transparent" asChild>
+			<SheetTrigger asChild className="bg-transparent hover:border-transparent">
 				<Button variant="ghost">
 					<Plus />
 					Register device
--- server/src/services/deleteEventsService.ts
+++ server/src/services/deleteEventsService.ts
@@ -1,13 +1,13 @@
 import { getStateStats, getTypeStats } from '../utils.js';
 import { devices, filteredDevices } from './deviceService.js';
 import { clients } from '../models/clients.js';
-import { AutoEventType } from '../models/autoEventType.js';
 
-export const addDeviceDeleteEvent = () => {
+export const addDeviceDeleteEvent = (ids: string[]) => {
 	clients.forEach(client => {
-		client.write(`event: ${AutoEventType.Deleted}\n`);
+		client.write(`event: multipleDevicesDeleted\n`);
 		client.write(
 			`data: ${JSON.stringify({
+				event: ids,
 				stats: { state: getStateStats(filteredDevices, devices.length), type: getTypeStats(filteredDevices) },
 			})}\n\n`
 		);
