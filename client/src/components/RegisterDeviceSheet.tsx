7e8966fdc46b6b34a8db51871417dd362b31a0ac
abubnov-sf <andrii.b@scalefinal.com>
2024-12-08 18:02:27 +0100

    Add toast after creating device

--- client/src/components/RegisterDeviceSheet.tsx
+++ client/src/components/RegisterDeviceSheet.tsx
@@ -1,28 +1,33 @@
 import { useContext, useState } from 'react';
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet.tsx';
+import { useToast } from '@/hooks/useToast.ts';
 import { Button } from '@/components/ui/button.tsx';
 import { RegisterDeviceForm } from '@/components/RegisterDeviceForm.tsx';
-import { RegisterDeviceSchemaType } from '@/schemas.ts';
 import { Plus } from 'lucide-react';
 import { useMutation } from '@tanstack/react-query';
 import { registerDevice } from '@/api/registerDevice.ts';
 import { PaginatedDataContext } from '@/providers/PaginatedDataContext.ts';
+import { RegisterDeviceSchemaType } from '@/schemas.ts';
 
 export const RegisterDeviceSheet = () => {
-	const {
-		paginationData: { isFetching },
-	} = useContext(PaginatedDataContext);
+	const { toast } = useToast();
+
+	const { paginationData } = useContext(PaginatedDataContext);
+
 	const [isOpen, setIsOpen] = useState(false);
+
 	const registerDeviceMutation = useMutation({ mutationFn: registerDevice });
 
 	const onSubmit = (form: RegisterDeviceSchemaType) => {
-		registerDeviceMutation.mutate(form);
+		registerDeviceMutation.mutate(form, {
+			onSuccess: () => toast({ title: `Device '${form.name}' has successfully been created` }),
+		});
 		setIsOpen(false);
 	};
 	return (
 		<Sheet open={isOpen} onOpenChange={setIsOpen}>
 			<SheetTrigger asChild className="bg-transparent hover:border-transparent">
-				<Button variant="ghost" disabled={registerDeviceMutation.isPending || isFetching}>
+				<Button variant="ghost" disabled={registerDeviceMutation.isPending || paginationData.isFetching}>
 					<Plus />
 					Register device
 				</Button>
