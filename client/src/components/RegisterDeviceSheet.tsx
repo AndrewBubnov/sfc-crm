6fca98326d37be81be4ef22daab1947a13e59b3f
abubnov-sf <andrii.b@scalefinal.com>
2024-12-08 18:02:37 +0100

    Add toast after creating device

--- client/src/components/RegisterDeviceSheet.tsx
+++ client/src/components/RegisterDeviceSheet.tsx
@@ -24,6 +24,7 @@ export const RegisterDeviceSheet = () => {
 		});
 		setIsOpen(false);
 	};
+
 	return (
 		<Sheet open={isOpen} onOpenChange={setIsOpen}>
 			<SheetTrigger asChild className="bg-transparent hover:border-transparent">
