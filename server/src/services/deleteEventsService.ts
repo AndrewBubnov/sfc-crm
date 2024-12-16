a2db298cd5747c554b6cfa44b4e7c139da0e5de9
abubnov-sf <andrii.b@scalefinal.com>
2024-12-16 10:24:27 +0100

    Fix deleting multiple devices server bug

--- server/src/services/deleteEventsService.ts
+++ server/src/services/deleteEventsService.ts
@@ -4,16 +4,16 @@ import { clients } from '../models/clients.js';
 import { offsetLimits } from './filterService.js';
 
 export const addDeviceDeleteEvent = (ids: string[]) => {
-	const { filteredDevices, total } = filterDevices();
+	const filteredDevices = filterDevices();
 	clients.forEach(client => {
 		client.write(`event: multipleDevicesDeleted\n`);
 		client.write(
 			`data: ${JSON.stringify({
 				event: ids,
 				stats: { state: getStateStats(filteredDevices, devices.length), type: getTypeStats(filteredDevices) },
-				items: filterDevices().filteredDevices.slice(
+				items: filteredDevices.slice(
 					Math.max(offsetLimits.offset - offsetLimits.limit, 0),
-					Math.max(offsetLimits.offset, Math.min(offsetLimits.limit, filterDevices().filteredDevices.length))
+					Math.max(offsetLimits.offset, Math.min(offsetLimits.limit, filteredDevices.length))
 				),
 			})}\n\n`
 		);
