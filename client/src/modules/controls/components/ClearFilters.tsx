983c008b509277eca85b5f28a7868594aa50b839
abubnov-sf <andrii.b@scalefinal.com>
2024-12-22 13:18:09 +0100

    Add tags for filtered fields

--- client/src/modules/controls/components/ClearFilters.tsx
+++ client/src/modules/controls/components/ClearFilters.tsx
@@ -1,22 +1,49 @@
-import { useContext } from 'react';
+import { useContext, useState, AnimationEvent } from 'react';
 import { SearchParamsContext } from '@/providers/SearchParamsContext.ts';
 import { Button } from '@/ui/button.tsx';
-import { SearchX } from 'lucide-react';
+import { SearchX, X } from 'lucide-react';
+import { cn } from '@/lib/utils.ts';
+import { FilterField } from '@/types.ts';
 
 export const ClearFilters = () => {
-	const { filters, resetFilters } = useContext(SearchParamsContext);
+	const { filters, resetFilters, onFilterChange } = useContext(SearchParamsContext);
+
+	const [deletedField, setDeletedField] = useState<string>('');
+
+	const onTransitionEnd = (field: FilterField) => (evt: AnimationEvent<HTMLButtonElement>) => {
+		if (evt.target === evt.currentTarget && deletedField) {
+			onFilterChange({ field, search: '' });
+			setDeletedField('');
+		}
+	};
+
 	if (filters.some(filter => filter.search.length)) {
 		return (
-			<Button variant="ghost" onClick={resetFilters}>
-				<SearchX />
-				<span>Clear all filters:</span>
-				<span className="text-gray-500">
+			<div className="flex items-center">
+				<Button variant="ghost" onClick={resetFilters} className="hover:bg-transparent">
+					<SearchX />
+					<span>Clear all filters:</span>
+				</Button>
+				<div className="flex items-center gap-1">
 					{filters
 						.filter(filter => filter.search.length)
 						.map(el => el.field)
-						.join(', ')}
-				</span>
-			</Button>
+						.map(field => (
+							<Button
+								key={field}
+								variant="outline"
+								className={cn(
+									'text-gray-500 h-7 p-2 hover:border-gray-200 -mb-0.5 animate-tag-mount',
+									field === deletedField && 'animate-tag-unmount'
+								)}
+								onClick={() => setDeletedField(field)}
+								onAnimationEnd={onTransitionEnd(field)}
+							>
+								{field} <X className="-mb-1" />
+							</Button>
+						))}
+				</div>
+			</div>
 		);
 	}
 	return null;
